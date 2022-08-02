56ed116345cc3e9c9f4c8ff481544922d7cb3f01
wechman <damian.wechman.it@gmail.com>
2022-08-02 14:44:38 +0200

    An extra assert for no operator for libraries in ASTJsonExporter::visit

--- libsolidity/ast/ASTJsonExporter.cpp
+++ libsolidity/ast/ASTJsonExporter.cpp
@@ -346,7 +346,12 @@ bool ASTJsonExporter::visit(UsingForDirective const& _node)
 		attributes.emplace_back("functionList", move(functionList));
 	}
 	else
-		attributes.emplace_back("libraryName", toJson(*_node.functionsOrLibrary().front()));
+	{
+		solAssert(_node.functionsAndOperators().size() == 1);
+		auto const& functionAndOperator = _node.functionsAndOperators().front();
+		solAssert(!functionAndOperator.second.has_value());
+		attributes.emplace_back("libraryName", toJson(*(functionAndOperator.first)));
+	}
 	attributes.emplace_back("global", _node.global());
 
 	setJsonNode(_node, "UsingForDirective", move(attributes));
